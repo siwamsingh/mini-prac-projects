ee4cea57d39b07e5472bed7df91a109ef1738c0f
siwam <siwamsingh2003@gmail.com>
2023-12-08 11:49:54 +0530

    bug: fixed import problem

--- mini-projects/src/main.jsx
+++ mini-projects/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import PasswordGenerator from "./App.jsx";
+import PasswordGenerator from "./PasswordGenerator.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
