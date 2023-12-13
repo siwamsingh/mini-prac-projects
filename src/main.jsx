442342425d4d7bcbfac0713fcb71d5581927a226
siwam <siwamsingh2003@gmail.com>
2023-12-13 23:11:08 +0530

    code: theme switcher

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import PasswordGenerator from "./PasswordGenerator.jsx";
 import RgbGenerator from "./RgbGenerator.jsx";
 import CurrencyConvertor from "./CurrencyConvertor.jsx";
 import "./index.css";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <PasswordGenerator />
     <RgbGenerator/>
     <CurrencyConvertor/>
+    <App/>
   </React.StrictMode>
 );
